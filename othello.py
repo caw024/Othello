7c6a9454d44e6a730047c6e35fff8b8670a522f7
Calvin Aw <caw@stuy.edu>
2019-07-26 00:48:29 -0400

    start

---
+++ othello.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import sys, time
+
+def random_move(board):
+    pass
+
+#changes a board based on moved position
+def change(board,move):
+    pass
+
+#mini max that foreshadows next move by points
+def naive_mini_max(board):
+    pass
+
+#mini max that foreshadows next n moves
+def mini_max(board, n):
+    pass
+
+#othello takes a string of 64 characters, b -> black, w -> white
+#index 0 to 63, from top left to bottom right
+def othello(board):
+    pass
+
+#day 1: implement change + random_move
+#day 2: implement naive mini max
+#day 3: implement mini max
+
+s = '_'*64
+othello(s)
